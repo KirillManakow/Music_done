82aba172150b5f429870c61aa5c43d0e347a17c0
KirillManakow <105341960+KirillManakow@users.noreply.github.com>
2022-12-18 19:41:49 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/app/audio/MainActivity.java
+++ app/src/main/java/com/example/app/audio/MainActivity.java
@@ -44,7 +44,7 @@ import com.example.app.audioplayer.R;
 public class MainActivity extends AppCompatActivity {
 
     public static final int REQUEST_ID_MULTIPLE_PERMISSIONS = 1;
-    public static final String Broadcast_PLAY_NEW_AUDIO = "com.valdioveliu.valdio.audioplayer.PlayNewAudio";
+    public static final String Broadcast_PLAY_NEW_AUDIO = "com.example.app.audioplayer.PlayNewAudio";
 
     private MediaPlayerService player;
     boolean serviceBound = false;
