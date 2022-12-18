01b2d9cf33d7e4934028e11e1610fba14f15093b
KirillManakow <105341960+KirillManakow@users.noreply.github.com>
2022-12-18 19:42:38 +0300

    Update MediaPlayerService.java

--- app/src/main/java/com/example/app/audio/MediaPlayerService.java
+++ app/src/main/java/com/example/app/audio/MediaPlayerService.java
@@ -39,11 +39,11 @@ public class MediaPlayerService extends Service implements MediaPlayer.OnComplet
         AudioManager.OnAudioFocusChangeListener {
 
 
-    public static final String ACTION_PLAY = "com.valdioveliu.valdio.audioplayer.ACTION_PLAY";
-    public static final String ACTION_PAUSE = "com.valdioveliu.valdio.audioplayer.ACTION_PAUSE";
-    public static final String ACTION_PREVIOUS = "com.valdioveliu.valdio.audioplayer.ACTION_PREVIOUS";
-    public static final String ACTION_NEXT = "com.valdioveliu.valdio.audioplayer.ACTION_NEXT";
-    public static final String ACTION_STOP = "com.valdioveliu.valdio.audioplayer.ACTION_STOP";
+    public static final String ACTION_PLAY = "com.example.app.audioplayer.ACTION_PLAY";
+    public static final String ACTION_PAUSE = "com.example.app.audioplayer.ACTION_PAUSE";
+    public static final String ACTION_PREVIOUS = "com.example.app.audioplayer.ACTION_PREVIOUS";
+    public static final String ACTION_NEXT = "com.example.app.audioplayer.ACTION_NEXT";
+    public static final String ACTION_STOP = "com.example.app.audioplayer.ACTION_STOP";
     private final String MEDIA_CHANNEL_ID = "media_playback_channel";
 
     private MediaPlayer mediaPlayer;
